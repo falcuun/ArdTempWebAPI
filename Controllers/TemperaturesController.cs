f280d6566cbcbe137eec229ca11a9d651842c009
falcuun <falcuun@gmail.com>
2019-01-01 20:45:14 +0100

    Fundamental Code laid down, Get method with dummy data made working. First commit

---
+++ Controllers/TemperaturesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TemperatureDataAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TemperatureDataAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TemperaturesController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}&{query}")]
+        public ActionResult Get(int id, string query)
+        {
+            Temperature temp = new Temperature();
+            temp.id = id;
+            temp.value = query;
+            
+            return new JsonResult(temp);
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Temperature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemperatureDataAPI.Models
+{
+    public class Temperature
+    {
+        public int id { set; get; }
+        public string value { set; get; }
+    }
+}
